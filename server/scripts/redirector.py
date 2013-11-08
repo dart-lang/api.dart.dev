1b7e62bede07983fb9e8affdaa41bcc128b93256
Martin Kustermann <kustermann@google.com>
2013-11-08 10:55:54 +0100

    Add channel support to api.dartlang.org
    This CL adds the following URL schemes:
      - http://api.dartlang.org/docs/channels/{be,dev,stable}/latest/
      - http://api.dartlang.org/docs/channels/{be,dev,stable}/424242/
    
    Our channel builders are archiving the dartdocs to the following location:
    gs://dartlang-api-docs/channels/{be,dev,stable}/REVISION/

--- server/scripts/redirector.py
+++ server/scripts/redirector.py
@@ -14,16 +14,26 @@ from google.appengine.api import files, memcache
 LATEST_CONTINUOUS_VERSION_FILE = '/gs/dart-editor-archive-continuous/latest/VERSION'
 LATEST_RELEASE_VERSION_FILE = '/gs/dart-editor-archive-integration/latest/VERSION'
 LATEST_TRUNK_VERSION_FILE = '/gs/dart-editor-archive-trunk/latest/VERSION'
+
+LATEST_BE_CHANNEL_VERSION_FILE = '/gs/dart-archive/channels/be/raw/latest/VERSION'
+LATEST_DEV_CHANNEL_VERSION_FILE = '/gs/dart-archive/channels/dev/release/latest/VERSION'
+LATEST_STABLE_CHANNEL_VERSION_FILE = '/gs/dart-archive/channels/stable/release/latest/VERSION'
+
 ONE_HOUR = 60 * 60
 ONE_DAY = ONE_HOUR * 24
 ONE_WEEK = ONE_DAY * 7
 
 class ApiDocs(blobstore_handlers.BlobstoreDownloadHandler):
-
-  latest_doc_version = None
-  latest_release_doc_version = None
-  latest_trunk_doc_version = None
   next_doc_version_check = None
+
+  latest_versions = {
+    'latest_doc_version': None,
+    'latest_release_doc_version': None,
+    'latest_trunk_doc_version': None,
+    'latest_be_doc_version': None,
+    'latest_dev_doc_version': None,
+    'latest_stable_doc_version': None,
+  }
 
   def reload_latest_version(self, version_file_location):
     data = None
@@ -53,32 +63,104 @@ class ApiDocs(blobstore_handlers.BlobstoreDownloadHandler):
       age = ONE_HOUR
     return age
 
-  def build_path(self, prefix, version_num):
-    return self.request.path.replace(prefix,
-          '/gs/dartlang-api-docs/' + str(version_num))
+  def build_gcs_path(self, version_num, postfix, channel=None):
+    if channel:
+      if version_num:
+        return '/gs/dartlang-api-docs/channels/%s/%s%s' % (
+            channel, version_num, postfix)
+      else:
+        return '/gs/dartlang-api-docs/channels/%s%s' % (
+            channel, postfix)
+    else:
+      return '/gs/dartlang-api-docs/%s%s' % (version_num, postfix)
 
   def resolve_doc_path(self):
-    if self.request.path.startswith('/docs/bleeding_edge'):
-      version_num = self.get_latest_version(ApiDocs.latest_doc_version, LATEST_CONTINUOUS_VERSION_FILE)
-      ApiDocs.latest_doc_version = version_num
-      path = self.build_path('/docs/bleeding_edge', version_num)
-    elif self.request.path.startswith('/docs/releases/latest'):
-      version_num = self.get_latest_version(ApiDocs.latest_release_doc_version, LATEST_RELEASE_VERSION_FILE)
-      ApiDocs.latest_release_doc_version = version_num
-      path = self.build_path('/docs/releases/latest', version_num)
-    elif self.request.path.startswith('/docs/trunk/latest'):
-      version_num = self.get_latest_version(ApiDocs.latest_trunk_doc_version, LATEST_TRUNK_VERSION_FILE)
-      ApiDocs.latest_trunk_doc_version = version_num
-      path = self.build_path('/docs/trunk/latest', version_num)
+    docs_renames = [
+      {
+        'key': 'latest_doc_version',
+        'prefix': '/docs/bleeding_edge',
+        'version_file': LATEST_CONTINUOUS_VERSION_FILE,
+      },
+      {
+        'key': 'latest_release_doc_version',
+        'prefix': '/docs/releases/latest',
+        'version_file': LATEST_RELEASE_VERSION_FILE,
+      },
+      {
+        'key': 'latest_trunk_doc_version',
+        'prefix': '/docs/trunk/latest',
+        'version_file': LATEST_TRUNK_VERSION_FILE,
+      },
+      {
+        'key': 'latest_be_doc_version',
+        'prefix': '/docs/channels/be/latest',
+        'version_file': LATEST_BE_CHANNEL_VERSION_FILE,
+        'channel': 'be',
+      },
+      {
+        'prefix': '/docs/channels/be',
+        'channel': 'be',
+        'manual_revision': True,
+      },
+      {
+        'key': 'latest_dev_doc_version',
+        'prefix': '/docs/channels/dev/latest',
+        'version_file': LATEST_DEV_CHANNEL_VERSION_FILE,
+        'channel': 'dev',
+      },
+      {
+        'prefix': '/docs/channels/dev',
+        'channel': 'dev',
+        'manual_revision': True,
+      },
+      {
+        'key': 'latest_stable_doc_version',
+        'prefix': '/docs/channels/stable/latest',
+        'version_file': LATEST_STABLE_CHANNEL_VERSION_FILE,
+        'channel': 'stable',
+      },
+      {
+        'prefix': '/docs/channels/stable',
+        'channel': 'stable',
+        'manual_revision': True,
+      },
+    ]
 
-    if path.endswith('/'):
+    path = None
+    for rename in docs_renames:
+      prefix = rename['prefix']
+      if self.request.path.startswith(prefix):
+        key = rename.get('key', None)
+        version_file = rename.get('version_file', None)
+        channel = rename.get('channel', None)
+        manual_revision = rename.get('manual_revision', False)
+
+        postfix = self.request.path[len(prefix):]
+
+        # If the URL doesn't include '/latest', we assume it's starting with a
+        # revision number, so we don't prepend the gcs path with the latest
+        # revision.
+        if manual_revision:
+          version_num = None
+        else:
+          version_num = self.get_latest_version(
+              ApiDocs.latest_versions[key], version_file)
+          ApiDocs.latest_versions[key] = version_num
+        path = self.build_gcs_path(version_num, postfix, channel=channel)
+        break
+
+    if path and path.endswith('/'):
       path = path + 'index.html'
     return path
 
   def get(self):
-    path = self.resolve_doc_path()
-    gs_key = blobstore.create_gs_key(path)
-    age = self.get_cache_age(path)
+    gcs_path = self.resolve_doc_path()
+    if not gcs_path:
+      self.error(404)
+      return
+
+    gs_key = blobstore.create_gs_key(gcs_path)
+    age = self.get_cache_age(gcs_path)
 
     self.response.headers['Cache-Control'] = 'max-age=' + \
         str(age) + ',s-maxage=' + str(age)
@@ -87,22 +169,22 @@ class ApiDocs(blobstore_handlers.BlobstoreDownloadHandler):
     # AE will serve a 500 if the file doesn't exist, but that should
     # be a 404
 
-    path_exists = memcache.get(path)
+    path_exists = memcache.get(gcs_path)
     if path_exists is not None:
       if path_exists == "1":
         self.send_blob(gs_key)
       else:
-        logging.debug('Memcache said ' + path + ' does not exist, sending 404')
+        logging.debug('Memcache said ' + gcs_path + ' does not exist, sending 404')
         self.error(404)
     else:
       try:
         # just check for existance
-        files.open(path, 'r').close()
-        memcache.add(key=path, value="1", time=ONE_DAY)
+        files.open(gcs_path, 'r').close()
+        memcache.add(key=gcs_path, value="1", time=ONE_DAY)
         self.send_blob(gs_key)
       except files.file.ExistenceError:
-        memcache.add(key=path, value="0", time=ONE_DAY)
-        logging.debug('Could not open ' + path + ', sending 404')
+        memcache.add(key=gcs_path, value="0", time=ONE_DAY)
+        logging.debug('Could not open ' + gcs_path + ', sending 404')
         self.error(404)
 
   # this doesn't get called, unfortunately.
@@ -112,9 +194,9 @@ class ApiDocs(blobstore_handlers.BlobstoreDownloadHandler):
   #   # awful hack for when file in cloud storage doesn't exist
   #   if isinstance(exception, TypeError):
   #     logging.debug('oh noes!')
-  #     path = self.resolve_path()
+  #     gcs_path = self.resolve_doc_path()
   #     try:
-  #       with files.open(path, 'r') as f:
+  #       with files.open(gcs_path, 'r') as f:
   #         # the file really does exist, so 500 must be something else
   #         self.error(500)
   #     except files.file.ExistenceError:
