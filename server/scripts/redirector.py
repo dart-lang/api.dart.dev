1dbf57030f2d4cbeb1d84cecf5bd8353fa054519
efortuna@google.com <efortuna@google.com>
2014-05-28 11:12:41 -0700

    Massive cleanup and refactoring, and also enable linking to a specific version of an SDK.
    The real changes are everything outside of the "out" directory. I updated the
    compiled version of the out directory, because the current plain checkout
    doesn't run out-of-the-box.
    
    Please see actual review at: https://codereview.chromium.org//297293008
    
    BUG=
    
    Review URL: https://codereview.chromium.org//298333006

--- server/scripts/redirector.py
+++ server/scripts/redirector.py
@@ -11,133 +11,64 @@ from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.api import files, memcache
 
-LATEST_BE_CHANNEL_VERSION_FILE = '/gs/dart-archive/channels/be/raw/latest/VERSION'
-LATEST_DEV_CHANNEL_VERSION_FILE = '/gs/dart-archive/channels/dev/release/latest/VERSION'
-LATEST_STABLE_CHANNEL_VERSION_FILE = '/gs/dart-archive/channels/stable/release/latest/VERSION'
-
 ONE_HOUR = 60 * 60
 ONE_DAY = ONE_HOUR * 24
 ONE_WEEK = ONE_DAY * 7
 
+
+class VersionInfo(object):
+  """Small helper class holding information about the last version seen and the
+  last time the version was checked for."""
+  def __init__(self, update_interval):
+    # The most recent version for this channel.
+    self.version = None
+    # The time this version was found.
+    self.last_check = None
+    self.update_interval = update_interval
+
+  def should_update(self):
+    """Tests to see if the last check was long enough past the update interval
+    that we should update the version."""
+    return datetime.now() > self.last_check + self.update_interval
+
 class ApiDocs(blobstore_handlers.BlobstoreDownloadHandler):
-  next_doc_version_check = None
+  GOOGLE_STORAGE = '/gs/dartlang-api-docs/channels'
 
+  def version_file_loc(self, channel):
+    return '%s/%s/latest.txt' % (ApiDocs.GOOGLE_STORAGE, channel)
+
+  # String to indicate we're looking for a specific version number/name.
+  VERSION_DIRECTORY = '/buildversion/'
+
+  # Dictionary of versions holding version information of the latest recorded
+  # version number and the time when it was recorded.
   latest_versions = {
-    'latest_be_doc_version': None,
-    'latest_dev_doc_version': None,
-    'latest_stable_doc_version': None,
-    'latest_be_docgen_version': None,
-    'latest_dev_docgen_version': None,
-    'latest_stable_docgen_version': None,
+    'be': VersionInfo(timedelta(minutes=30)),
+    'dev': VersionInfo(timedelta(hours=6)),
+    'stable': VersionInfo(timedelta(days=1)),
   }
 
-  latest_version_names = {
-    'latest_be_doc_version': None,
-    'latest_dev_doc_version': None,
-    'latest_stable_doc_version': None,
-    'latest_be_docgen_version': None,
-    'latest_dev_docgen_version': None,
-    'latest_stable_docgen_version': None,
-  }
-
-  docs_renames = [
-    {
-      'key': 'latest_be_doc_version',
-      'prefix': '/docs/channels/be/latest',
-      'version_file': LATEST_BE_CHANNEL_VERSION_FILE,
-      'channel': 'be',
-    },
-    {
-      'key': 'latest_be_docgen_version',
-      'prefix': '/apidocs/channels/be/docs',
-      'version_file': LATEST_BE_CHANNEL_VERSION_FILE,
-      'channel': 'be',
-      'docgen' : True,
-    },
-    {
-      'prefix': '/docs/channels/be',
-      'channel': 'be',
-      'manual_revision': True,
-    },
-    {
-      'prefix': '/apidocs/channels/be',
-      'channel': 'be',
-      'manual_revision': True,
-      'docgen' : True,
-    },
-    {
-      'key': 'latest_dev_doc_version',
-      'prefix': '/docs/channels/dev/latest',
-      'version_file': LATEST_DEV_CHANNEL_VERSION_FILE,
-      'channel': 'dev',
-    },
-    {
-      'prefix': '/docs/channels/dev',
-      'channel': 'dev',
-      'manual_revision': True,
-    },
-    {
-      'key': 'latest_dev_docgen_version',
-      'prefix': '/apidocs/channels/dev/docs',
-      'version_file': LATEST_DEV_CHANNEL_VERSION_FILE,
-      'channel': 'dev',
-      'docgen' : 'true',
-    },
-    {
-      'prefix': '/apidocs/channels/dev',
-      'channel': 'dev',
-      'manual_revision': True,
-      'docgen' : True,
-    },
-    {
-      'key': 'latest_stable_doc_version',
-      'prefix': '/docs/channels/stable/latest',
-      'version_file': LATEST_STABLE_CHANNEL_VERSION_FILE,
-      'channel': 'stable',
-    },
-    {
-      'key': 'latest_stable_docgen_version',
-      'prefix': '/apidocs/channels/stable/docs',
-      'version_file': LATEST_STABLE_CHANNEL_VERSION_FILE,
-      'channel': 'stable',
-      'docgen' : True,
-    },
-    {
-      'prefix': '/docs/channels/stable',
-        'channel': 'stable',
-        'manual_revision': True,
-    },
-    {
-      'prefix': '/apidocs/channels/stable',
-        'channel': 'stable',
-        'manual_revision': True,
-        'docgen' : True,
-    },
-  ]
-
-  def reload_latest_version(self, version_file_location, version_key):
+  def reload_latest_version(self, channel):
+    """Check Google storage to determine the latest version file in a given
+    channel."""
     data = None
+    version_file_location = self.version_file_loc(channel)
     with files.open(version_file_location, 'r') as f:
       data = json.loads(f.read(1024))
-      ApiDocs.next_doc_version_check = datetime.now() + timedelta(days=1)
-    revision = int(data['revision'])
-    version = data['version']
-    ApiDocs.latest_versions[version_key] = revision
-    ApiDocs.latest_version_names[version_key] = version
+      ApiDocs.latest_versions[channel].last_check = datetime.now()
+    revision = int(data)
+    ApiDocs.latest_versions[channel].version = revision
     return revision
 
-  # TODO: put into memcache?
-  def get_latest_version(self, version, version_file_location, version_key):
-    forced_reload = self.request.get('force_reload')
-    if (forced_reload or
-          version is None or
-          version_file_location == LATEST_BE_CHANNEL_VERSION_FILE or
-          ApiDocs.next_doc_version_check is None or
-          datetime.now() > ApiDocs.next_doc_version_check):
-      new_version = self.reload_latest_version(version_file_location, version_key)
-      return new_version
+  def get_latest_version(self, channel):
+    """Determine what the latest version number is for this particular channel.
+    We do a bit of caching so that we're not constantly pinging for the latest
+    version of stable, for example."""
+    version_info = ApiDocs.latest_versions[channel]
+    if (version_info.version is None or version_info.should_update()):
+      return self.reload_latest_version(channel)
     else:
-      return version
+      return version_info.version
 
   def get_cache_age(self, path):
     if re.search(r'(png|jpg)$', path):
@@ -148,109 +79,90 @@ class ApiDocs(blobstore_handlers.BlobstoreDownloadHandler):
       age = ONE_HOUR
     return age
 
-  def build_gcs_path(self, version_num, postfix, channel=None):
-    if channel:
-      if version_num:
-        return '/gs/dartlang-api-docs/channels/%s/%s%s' % (
-            channel, version_num, postfix)
-      else:
-        return '/gs/dartlang-api-docs/channels/%s%s' % (
-            channel, postfix)
-    else:
-      return '/gs/dartlang-api-docs/%s%s' % (version_num, postfix)
+  def build_gcs_path(self, version_num, postfix, channel):
+    """Build the path to the information on Google Storage."""
+    return '%s/%s/%s/docgen%s' % (ApiDocs.GOOGLE_STORAGE, channel, version_num,
+        postfix)
 
-  def resolve_doc_path(self):
+  def resolve_doc_path(self, channel):
+    """Given the request URL, determine what specific docs version we should
+    actually display."""
     path = None
-    for rename in self.docs_renames:
-      prefix = rename['prefix']
-      if self.request.path.startswith(prefix):
-        key = rename.get('key', None)
-        version_file = rename.get('version_file', None)
-        channel = rename.get('channel', None)
-        manual_revision = rename.get('manual_revision', False)
-
-        postfix = self.request.path[len(prefix):]
-        if (rename.get('docgen', False)) :
-          postfix = "/docgen" + postfix
-
-        # If the URL doesn't include '/latest', we assume it's starting with a
-        # revision number, so we don't prepend the gcs path with the latest
-        # revision.
-        if manual_revision:
-          version_num = None
-        else:
-          version_num = self.get_latest_version(
-              ApiDocs.latest_versions[key], version_file, key)
-        path = self.build_gcs_path(version_num, postfix, channel=channel)
-        break
+    if channel:
+      prefix = '/apidocs/channels/%s/docs' % channel
+      postfix = self.request.path[len(prefix):]
+      if postfix.startswith(ApiDocs.VERSION_DIRECTORY):
+        postfix = postfix[len(ApiDocs.VERSION_DIRECTORY):]
+        index = postfix.find('/')
+        version_num = postfix[:index]
+        postfix = postfix[index:]
+      else:
+        version_num = self.get_latest_version(channel)
+      path = self.build_gcs_path(version_num, postfix, channel)
 
     if path and path.endswith('/'):
       path = path + 'index.html'
     return path
 
+  def get_channel(self):
+    """Quick accessor to examine a request and determine what channel
+    (dev/stable/etc) we're looking at. Return None if we have a weird unexpected
+    URL."""
+    for channel in ApiDocs.latest_versions.keys():
+      if self.request.path.startswith('/apidocs/channels/%s' % channel):
+        return channel
+
   def get(self, *args, **kwargs):
-    versionRequest = kwargs.pop('_versionRequest', None)
-    if versionRequest:
-      version_file_entry = [x for x in self.docs_renames if x.get('key') == versionRequest]
-      version_file = version_file_entry[0]['version_file']
-      self.get_latest_version(
-          ApiDocs.latest_versions[versionRequest], version_file, versionRequest)
-      self.response.text = ApiDocs.latest_version_names[versionRequest]
-      return
-    gcs_path = self.resolve_doc_path()
-    if not gcs_path:
-      self.error(404)
-      return
+    """The main entry point for handling the URL for those with ApiDocs as the
+    handler. See http://webapp-improved.appspot.com/api/webapp2.html?highlight=
+    redirecthandler#webapp2.RedirectHandler.get.
 
-    gs_key = blobstore.create_gs_key(gcs_path)
-    age = self.get_cache_age(gcs_path)
-
-    self.response.headers['Cache-Control'] = 'max-age=' + \
-        str(age) + ',s-maxage=' + str(age)
-
-    # is there a better way to check if a file exists in cloud storage?
-    # AE will serve a 500 if the file doesn't exist, but that should
-    # be a 404
-
-    path_exists = memcache.get(gcs_path)
-    if path_exists is not None:
-      if path_exists == "1":
-        self.send_blob(gs_key)
-      else:
-        logging.debug('Memcache said ' + gcs_path + ' does not exist, sending 404')
-        self.error(404)
+    Arguments:
+    - args: Positional arguments passed to this URL handler
+    - kwargs: Dictionary arguments passed to the hander; expecting at least one
+      item in the dictionary with a key of 'path', which was populated from the
+      regular expression matching in Route."""
+    channel = self.get_channel()
+    if (channel and self.request.path[len('/apidocs/channels/%s/docs' %
+        channel):] == '/latest.txt'):
+      self.response.text = unicode(self.get_latest_version(channel))
     else:
-      try:
-        # just check for existance
-        files.open(gcs_path, 'r').close()
-        memcache.add(key=gcs_path, value="1", time=ONE_DAY)
-        self.send_blob(gs_key)
-      except files.file.ExistenceError:
-        memcache.add(key=gcs_path, value="0", time=ONE_DAY)
-        logging.debug('Could not open ' + gcs_path + ', sending 404')
+      gcs_path = self.resolve_doc_path(channel)
+      if not gcs_path:
         self.error(404)
+        return
 
-  # this doesn't get called, unfortunately.
-  # if this ever starts working, remove the try and files.open
-  # from get, above, and instead retroactively handle a missing file here
-  # def handle_exception(self, exception, debug_mode):
-  #   # awful hack for when file in cloud storage doesn't exist
-  #   if isinstance(exception, TypeError):
-  #     logging.debug('oh noes!')
-  #     gcs_path = self.resolve_doc_path()
-  #     try:
-  #       with files.open(gcs_path, 'r') as f:
-  #         # the file really does exist, so 500 must be something else
-  #         self.error(500)
-  #     except files.file.ExistenceError:
-  #       # file does not exist
-  #       self.error(404)
-  #   else:
-  #     logging.exception(exception)
+      gs_key = blobstore.create_gs_key(gcs_path)
+      age = self.get_cache_age(gcs_path)
+
+      self.response.headers['Cache-Control'] = 'max-age=' + \
+          str(age) + ',s-maxage=' + str(age)
+
+      # is there a better way to check if a file exists in cloud storage?
+      # AE will serve a 500 if the file doesn't exist, but that should
+      # be a 404
+
+      path_exists = memcache.get(gcs_path)
+      if path_exists is not None:
+        if path_exists == "1":
+          self.send_blob(gs_key)
+        else:
+          logging.debug('Memcache said ' + gcs_path +
+              ' does not exist, sending 404')
+          self.error(404)
+      else:
+        try:
+          # just check for existence
+          files.open(gcs_path, 'r').close()
+          memcache.add(key=gcs_path, value="1", time=ONE_DAY)
+          self.send_blob(gs_key)
+        except files.file.ExistenceError:
+          memcache.add(key=gcs_path, value="0", time=ONE_DAY)
+          logging.debug('Could not open ' + gcs_path + ', sending 404')
+          self.error(404)
 
 def redir_to_latest(handler, *args, **kwargs):
   path = kwargs['path']
-  # Should be #! if we use that scheme.
   return '/apidocs/channels/stable/dartdoc-viewer/home'
 
 def redir_dom(handler, *args, **kwargs):
@@ -272,13 +184,14 @@ def redir_docgen_stable(handler, *args, **kwargs):
   return '/docs/channels/stable/latest/docgen' + kwargs['path']
 
 def redir_pkgs(handler, *args, **kwargs):
-  # Should be #! if we use that scheme
   return '/apidocs/channels/stable/dartdoc-viewer/' + kwargs['pkg']
 
 # Redirect old apidoc URIs
 def redir_old(kwargs, channel):
+  """Crufty old code that hasn't been touched in a long time. Still here for
+  legacy reasons to not break old links. :-/ Let sleeping dogs lie?"""
   old_path = kwargs['path'][1:]
-  if (old_path == ''): 
+  if (old_path == ''):
     return '/apidocs/channels/stable/dartdoc-viewer/home'
   split = old_path.split('/')
   firstPart = split[0]
@@ -293,7 +206,7 @@ def redir_old(kwargs, channel):
     'utf']
   if firstPart in packages:
     prefix = firstPart + '/' + firstPart
-  else: 
+  else:
     prefix = firstPart.replace('_', ':', 1).replace('.html', '')
     # For old URLs like core/String.html. We know it's not a package, so
     # it ought to start with a dart: library
@@ -314,18 +227,18 @@ def redir_old_stable(handler, *args, **kwargs):
 
 application = WSGIApplication(
   [
-    Route('/docs/pkg/<pkg:args|crypto|custom_element|fixnum|http_server|intl|json|logging|matcher|mime|mock|observe|path|polymer|polymer_expressions|sequence_zip|serialization|source_maps|template_binding|unittest|unmodifiable_collection|utf><:/?>',
+    # Legacy URL redirection schemes.
+    # Redirect all old URL package requests to our updated URL scheme.
+    # TODO(efortuna): Remove this line when pkg gets moved off of
+    # api.dartlang.org.
+    Route('/docs/pkg/<pkg:args|crypto|custom_element|fixnum|http_server|intl|'
+        'json|logging|matcher|mime|mock|observe|path|polymer|'
+        'polymer_expressions|sequence_zip|serialization|source_maps|'
+        'template_binding|unittest|unmodifiable_collection|utf><:/?>',
         RedirectHandler, defaults={'_uri': redir_pkgs, '_code': 302}),
     Route('/dom<path:.*>', RedirectHandler, defaults={'_uri': redir_dom}),
-    Route('/docs/bleeding_edge<path:.*>', RedirectHandler, defaults={'_uri': redir_continuous}),
-
-    # If it requests a VERSION file, just serve up the version from our internal storage.
-    Route('/apidocs/channels/be/docs/VERSION', 
-        ApiDocs, defaults={'_versionRequest' : 'latest_be_doc_version'}),
-    Route('/apidocs/channels/dev/docs/VERSION', 
-        ApiDocs, defaults={'_versionRequest' : 'latest_dev_doc_version'}),
-    Route('/apidocs/channels/stable/docs/VERSION', 
-        ApiDocs, defaults={'_versionRequest' : 'latest_stable_doc_version'}),
+    Route('/docs/bleeding_edge<path:.*>', RedirectHandler,
+        defaults={'_uri': redir_continuous}),
 
     # Data requests go to cloud storage
     Route('/apidocs/channels/be/docs<path:.*>', ApiDocs),
@@ -333,25 +246,31 @@ application = WSGIApplication(
     Route('/apidocs/channels/stable/docs<path:.*>', ApiDocs),
 
     # Add the trailing / if necessary.
-    Route('/docs/channels/be', RedirectHandler, defaults={'_uri': '/apidocs/channels/be/'}),
-    Route('/docs/channels/dev', RedirectHandler, defaults={'_uri': '/apidocs/channels/dev/'}),
-    Route('/docs/channels/stable', RedirectHandler, defaults={'_uri': '/apidocs/channels/stable/'}),
-    Route('/apidocs/channels/be/dartdoc-viewer', RedirectHandler, 
+    Route('/apidocs/channels/be/dartdoc-viewer', RedirectHandler,
         defaults={'_uri': '/apidocs/channels/be/dartdoc-viewer/'}),
-    Route('/apidocs/channels/dev/dartdoc-viewer', RedirectHandler, 
+    Route('/apidocs/channels/dev/dartdoc-viewer', RedirectHandler,
         defaults={'_uri': '/apidocs/channels/dev/dartoc-viewer/'}),
-    Route('/apidocs/channels/stable/dartdoc-viewer', RedirectHandler, 
+    Route('/apidocs/channels/stable/dartdoc-viewer', RedirectHandler,
         defaults={'_uri': '/apidocs/channels/stable/dartdoc-viewer/'}),
 
-    Route('/docs/continuous<path:.*>', RedirectHandler, defaults={'_uri': redir_continuous}),
-    Route('/docs/releases/latest<path:.*>', RedirectHandler, defaults={'_uri': redir_latest}),
+    Route('/docs/continuous<path:.*>', RedirectHandler,
+        defaults={'_uri': redir_continuous}),
+    Route('/docs/releases/latest<path:.*>', RedirectHandler,
+        defaults={'_uri': redir_latest}),
 
-     # Redirect apidoc links
-    Route('/docs/channels/be/latest<path:.*>', RedirectHandler, defaults={'_uri': redir_old_be}),
-    Route('/docs/channels/dev/latest<path:.*>', RedirectHandler, defaults={'_uri': redir_old_dev}),
-    Route('/docs/channels/stable/latest<path:.*>', RedirectHandler, defaults={'_uri': redir_old_stable}),
-
-    ('/docs.*', ApiDocs),
+     # Legacy handling: redirect old doc links to apidoc.
+    Route('/docs/channels/be/latest<path:.*>', RedirectHandler,
+        defaults={'_uri': redir_old_be}),
+    Route('/docs/channels/dev/latest<path:.*>', RedirectHandler,
+        defaults={'_uri': redir_old_dev}),
+    Route('/docs/channels/stable/latest<path:.*>', RedirectHandler,
+        defaults={'_uri': redir_old_stable}),
+    Route('/docs/channels/be', RedirectHandler,
+        defaults={'_uri': '/apidocs/channels/be/'}),
+    Route('/docs/channels/dev', RedirectHandler,
+        defaults={'_uri': '/apidocs/channels/dev/'}),
+    Route('/docs/channels/stable', RedirectHandler,
+        defaults={'_uri': '/apidocs/channels/stable/'}),
 
     Route('<path:.*>', RedirectHandler, defaults={'_uri': redir_old_stable})
   ],
